438bbf44d297f23a95dcbecc429e3cab154e5362
thejamilah <jamilahmcw@gmail.com>
2018-10-15 16:46:22 -0700

    initial commit

--- Lab05_OOP_Principles/Lab05_OOP_Principles/Program.cs
+++ Lab05_OOP_Principles/Lab05_OOP_Principles/Program.cs
@@ -6,7 +6,7 @@ namespace Lab05_OOP_Principles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Building a Zoo");
         }
     }
 }
