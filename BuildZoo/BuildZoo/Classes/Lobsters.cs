a9e8a8fb8c425eb6fd7dfd1a7bf1ed839a2b2c11
thejamilah <jamilahmcw@gmail.com>
2018-10-22 21:12:41 -0700

    working late

--- BuildZoo/BuildZoo/Classes/Lobsters.cs
+++ BuildZoo/BuildZoo/Classes/Lobsters.cs
@@ -2,39 +2,7 @@
 
 namespace BuildZoo.Classes.NonVertabrates.HasLegs.Crustacea
 {
-    class Lobsters : Aquatic, IComparable<Lobsters>
+    public class Lobsters 
     {
-        public float weight;
-
-        public int CompareTo(Lobsters other)
-        {
-            return weight.CompareTo(other.weight);//-1,0 or 1
-        }
-
-
-        public override bool AreMultiCellular { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public override bool AreHeterotrophs { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
-        public override string Are()
-        {
-           return "Are delicious!";
-        }
-
-        public override bool canFly()
-        {
-            return CanFly();
-        }
-
-        
-
-        public override string Eat()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override bool LiveInWater()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
